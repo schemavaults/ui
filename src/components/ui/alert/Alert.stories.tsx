66b085efaf19e7a87ec94e7f7b4d747528169eed
jalexw <jalexwhitman@gmail.com>
2025-06-14 14:24:47 -0300

    0.11.21 - story for alert component

---
+++ src/components/ui/alert/Alert.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from "@storybook/react";
+// import { fn } from "@storybook/test";
+
+import {
+  Alert,
+  AlertTitle,
+  AlertDescription,
+  alertVariantIds,
+  type AlertVariantId,
+} from "./alert";
+import type { ReactElement } from "react";
+
+interface AlertExampleProps {
+  variant?: AlertVariantId;
+}
+
+function AlertExample(props: AlertExampleProps): ReactElement {
+  return (
+    <Alert {...props}>
+      <AlertTitle>{"This is an <Alert />"}</AlertTitle>
+      <AlertDescription>
+        This is a description for the alert containing more information about
+        why an alert is being shown!
+      </AlertDescription>
+    </Alert>
+  );
+}
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Components/Alert",
+  component: AlertExample,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    variant: {
+      options: alertVariantIds,
+      control: {
+        type: "radio",
+      },
+    },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof AlertExample>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const DefaultAlert: Story = {
+  args: {
+    variant: "default",
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+  },
+};
--- src/components/ui/alert/alert.tsx
+++ src/components/ui/alert/alert.tsx
@@ -5,6 +5,13 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { HTMLAttributes, ReactElement, Ref } from "react";
 
+export const alertVariantIds = [
+  "default",
+  "destructive",
+] as const satisfies string[];
+
+export type AlertVariantId = (typeof alertVariantIds)[number];
+
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
@@ -13,7 +20,7 @@ const alertVariants = cva(
         default: "bg-background text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-      },
+      } satisfies Record<AlertVariantId, string>,
     },
     defaultVariants: {
       variant: "default",
