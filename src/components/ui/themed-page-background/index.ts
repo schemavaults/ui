f3a8b028f431015c500fa95a41b3d8da8cbede10
jalexw <jalexwhitman@gmail.com>
2025-06-14 14:53:53 -0300

    0.11.25 - story for themed page background component

---
+++ src/components/ui/themed-page-background/index.ts
@@ -0,0 +1,3 @@
+export * from "./themed-page-background";
+export type * from "./themed-page-background";
+export { ThemedPageBackground as default } from "./themed-page-background";
---
+++ src/components/ui/themed-page-background/themed-page-background.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { PropsWithChildren, ReactElement } from "react";
+import { cn } from "@/lib/utils";
+
+export interface ThemedPageBackgroundProps extends PropsWithChildren {
+  className?: string;
+}
+
+export function ThemedPageBackground({
+  className,
+  children,
+}: ThemedPageBackgroundProps): ReactElement {
+  return (
+    <div className="bg-blue-500 min-h-screen w-full -z-10">
+      <div className="absolute inset-0 bg-gradient-to-b from-red-500  to-transparent" />
+      <div className={cn("relative z-10 w-full min-h-screen", className)}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default ThemedPageBackground;
