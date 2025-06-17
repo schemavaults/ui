ce4d6faab6d64e25909b954a09b5250f309bc953
jalexw <jalexwhitman@gmail.com>
2025-06-17 12:38:01 -0300

    text-nowrap for wordmark

--- src/components/ui/wordmark/wordmark.tsx
+++ src/components/ui/wordmark/wordmark.tsx
@@ -11,7 +11,7 @@ export function Wordmark({ className }: WordmarkProps): ReactElement {
   return (
     <span
       className={cn(
-        "text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-red-600",
+        "text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-red-600 text-nowrap",
         className,
       )}
     >
