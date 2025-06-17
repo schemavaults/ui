0e80d6dabca98c68ef5abbeb011cf8e3602604a7
jalexw <jalexwhitman@gmail.com>
2025-06-17 17:57:07 -0300

    0.11.39 - story for text area component

---
+++ src/components/ui/textarea/index.ts
@@ -0,0 +1,3 @@
+export * from "./textarea";
+export type * from "./textarea";
+export { Textarea as default } from "./textarea";
--- src/components/ui/textarea/textarea.tsx
+++ src/components/ui/textarea/textarea.tsx
@@ -1,28 +1,25 @@
 "use client";
 
-import * as React from "react"
+import { type TextareaHTMLAttributes } from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = (
-  {
-    className,
-    ...props
-  }: TextareaProps
-) => {
+const Textarea = ({ className, ...props }: TextareaProps) => {
   return (
     <textarea
       className={cn(
         "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-        className
+        className,
       )}
       {...props}
     />
-  )
-}
-Textarea.displayName = "Textarea"
+  );
+};
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
+
+export default Textarea;
