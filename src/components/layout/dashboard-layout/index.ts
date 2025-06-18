16c5fcebe17f5440e5dfaaef37ced9fb0061fc11
jalexw <jalexwhitman@gmail.com>
2025-06-18 14:12:02 -0300

    0.11.56 - export dashboard item&group types for defining in external app

--- src/components/layout/dashboard-layout/index.ts
+++ src/components/layout/dashboard-layout/index.ts
@@ -2,7 +2,7 @@ export * from "./dashboard-layout";
 export type * from "./dashboard-layout";
 export { DashboardLayout as default } from "./dashboard-layout";
 
-// export { DashboardLayoutContextProvider } from "./dashboard-layout-context-provider";
-// export type * from "./dashboard-layout-context-provider";
-
 export type * from "./customizable-dashboard-component-type";
+export type { DashboardSidebarItemDefinition } from "./dashboard-sidebar-item-definition";
+export type { DashboardSidebarItemGroupDefinition } from "./dashboard-sidebar-item-group";
+export type { DashboardSidebarItemsAndGroupsDefinitions } from "./dashboard-sidebar-items-and-groups-context";
