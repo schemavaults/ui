bf0cb429843a055ceddf78cf99fb46e09237b9bb
jalexw <jalexwhitman@gmail.com>
2025-06-18 17:12:40 -0300

    0.11.61 - export themed page container to allow import without dist/ path

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -3,3 +3,6 @@ export type * from "./page-column-container";
 
 export * from "./dashboard-layout";
 export type * from "./dashboard-layout";
+
+export * from "./themed-page-container";
+export type * from "./themed-page-container";
---
+++ src/components/layout/themed-page-container/index.ts
@@ -0,0 +1,4 @@
+export * from "./themed-page-container";
+export type * from "./themed-page-container";
+
+export { ThemedPageContainer as default } from "./themed-page-container";
