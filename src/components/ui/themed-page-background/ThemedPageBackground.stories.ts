22ba0924619299c3332e0391f1fd5288ffb929c0
jalexw <jalexwhitman@gmail.com>
2025-06-19 15:34:06 -0300

    0.11.65 - make adding classnames to themedpagebackground more flexible

--- src/components/ui/themed-page-background/ThemedPageBackground.stories.ts
+++ src/components/ui/themed-page-background/ThemedPageBackground.stories.ts
@@ -19,10 +19,14 @@ const meta = {
     className: {
       control: "text",
     },
+    backgroundClassName: {
+      control: "text",
+    },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {
     className: "w-full min-h-screen",
+    backgroundClassName: "h-screen",
   } satisfies ThemedPageBackgroundProps,
 } satisfies Meta<typeof ThemedPageBackground>;
 
--- src/components/ui/themed-page-background/themed-page-background.tsx
+++ src/components/ui/themed-page-background/themed-page-background.tsx
@@ -4,7 +4,10 @@ import type { PropsWithChildren, ReactElement } from "react";
 import { cn } from "@/lib/utils";
 
 export interface ThemedPageBackgroundProps extends PropsWithChildren {
+  /** Append classnames to the content container within the background gradient container */
   className?: string;
+  /** Append classnames to the background gradient container */
+  backgroundClassName?: string;
 }
 
 /**
@@ -15,18 +18,21 @@ export interface ThemedPageBackgroundProps extends PropsWithChildren {
  * @returns A component that fills the background container with a colorful SchemaVaults-themed background; child content is displayed on top
  */
 export function ThemedPageBackground({
-  className,
   children,
+  ...props
 }: ThemedPageBackgroundProps): ReactElement {
+  const backgroundClassNames: string[] = [
+    "schemavaults-themed-page-background",
+    "relative min-h-full w-full",
+    "bg-schemavaults-brand-blue bg-gradient-to-b from-schemavaults-brand-red to-transparent",
+  ];
+  if (typeof props.backgroundClassName === "string") {
+    backgroundClassNames.push(props.backgroundClassName);
+  }
+
   return (
-    <div
-      className={[
-        "schemavaults-themed-page-background",
-        "relative min-h-full w-full",
-        "bg-schemavaults-brand-blue bg-gradient-to-b from-schemavaults-brand-red to-transparent",
-      ].join(" ")}
-    >
-      <div className={cn("relative z-10 w-full min-h-full", className)}>
+    <div className={backgroundClassNames.join(" ")}>
+      <div className={cn("relative z-10 w-full min-h-full", props.className)}>
         {children}
       </div>
     </div>
