85395fe1594126be1630cc5784105f2a11b6637e
jalexw <jalexwhitman@gmail.com>
2025-08-05 10:14:13 -0300

    0.11.94 - fix lazy framer export

--- src/components/providers/lazy_framer.tsx
+++ src/components/providers/lazy_framer.tsx
@@ -3,18 +3,16 @@
 import { domAnimation, LazyMotion } from "@/framer-motion";
 import type { PropsWithChildren, ReactElement } from "react";
 
-export { LazyMotion } from "@/framer-motion";
-
 export interface LazyFramerMotionProviderProps extends PropsWithChildren {
   strict?: boolean;
 }
 
 export function LazyFramerMotionProvider({
   children,
-  strict,
+  ...props
 }: LazyFramerMotionProviderProps): ReactElement {
   return (
-    <LazyMotion features={domAnimation} strict={strict}>
+    <LazyMotion features={domAnimation} strict={props.strict}>
       {children}
     </LazyMotion>
   );
