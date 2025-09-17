c6a4f116169f329c7c5eec98513f0beab03c92bc
jalexw <jalexwhitman@gmail.com>
2025-09-17 12:33:45 -0300

    0.11.121 - fix export icon component

--- src/components/ui/icon/index.ts
+++ src/components/ui/icon/index.ts
@@ -1,3 +1,3 @@
 export * from "./Icon";
-export { default as default } from "./Icon";
+export { default as default, Icon, DEFAULT_ICON_SIZE } from "./Icon";
 export type { IconProps } from "./IconProps";
