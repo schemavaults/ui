a6dc0a71004f11509b2b8c9feea6a98eec9ae7ce
jalexw <jalexwhitman@gmail.com>
2025-09-18 19:34:52 -0300

    0.11.123 - export svg dataurl prefix

--- src/components/ui/icon/Icon.tsx
+++ src/components/ui/icon/Icon.tsx
@@ -3,3 +3,5 @@
 export { Icon, Icon as default } from "./IconFacade";
 export type { IconProps } from "./IconProps";
 export { DEFAULT_ICON_SIZE } from "./DefaultIconSize";
+
+export { svgDataUrlPrefix } from "./SvgDataUrl";
