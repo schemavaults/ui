3fa2db9219cd6f62fb4d54d0d0d96f6a69920aa0
jalexw <jalexwhitman@gmail.com>
2025-10-01 20:46:41 -0300

    0.11.126 - document how to setup @schemavaults/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,63 +4,10 @@ import type { Config } from "tailwindcss";
 // Import the config factory
 import { SchemaVaultsTailwindConfigFactory } from "@schemavaults/theme";
 
-let currentDirectory: string;
-try {
-  currentDirectory = __dirname;
-  if (typeof currentDirectory !== "string") {
-    throw new Error("current dir path not a string");
-  }
-} catch (e: unknown) {
-  console.error("Failed to load current directory");
-  process.exit(1);
-}
-
-// Import helper utilities for file/module resolution
-import { join } from "path";
-import { existsSync, lstatSync, type Stats } from "fs";
-
-function node_modules(): string {
-  let node_modules_path: string;
-  if (currentDirectory.endsWith("/src") || currentDirectory.endsWith("/dist")) {
-    node_modules_path = join(currentDirectory, "..", "node_modules");
-  } else {
-    node_modules_path = join(currentDirectory, "node_modules");
-  }
-
-  return node_modules_path;
-}
-
-function isdir(path: string): boolean {
-  if (!existsSync(path)) return false;
-
-  let lstatResult: Stats;
-  try {
-    lstatResult = lstatSync(path);
-  } catch (e: unknown) {
-    throw new Error(
-      "Error running lstatSync for tailwind.config.ts isdir() function!",
-    );
-  }
-  return lstatResult.isDirectory();
-}
-
-if (!isdir(node_modules())) {
-  console.error("Failed to resolve node_modules/ directory for test!");
-  throw new Error("Failed to resolve node_modules/ directory for test!");
-}
-
 let config: Config;
 try {
-  // Initialize the config factory
-  const configFactory = new SchemaVaultsTailwindConfigFactory({
-    node_modules,
-    isdir,
-    join,
-    debug: true,
-  });
-
   // Generate and export the config
-  config = configFactory.createConfig({
+  config = new SchemaVaultsTailwindConfigFactory().createConfig({
     content: ["./src/**/*.{tsx,jsx,js,ts,mdx}"],
   }) satisfies Config;
 } catch (e) {
