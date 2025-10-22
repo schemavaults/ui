0244696be7726154b09d7fe48a57857d61ecddeb
jalexw <jalexwhitman@gmail.com>
2025-10-22 14:16:04 -0300

    simplify storybook theme setup

--- .storybook/SchemaVaultsTheme.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "@storybook/theming";
-
-export default create({
-  base: "light",
-  brandTitle: "@schemavaults/ui",
-  brandUrl: "https://ui.schemavaults.com",
-  brandImage: "https://ui.schemavaults.com/media/brand.png",
-  brandTarget: "_self",
-});
