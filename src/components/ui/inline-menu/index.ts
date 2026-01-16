07c8e91a716ab492585cd575c896d1183766d0a9
jalexw <jalexwhitman@gmail.com>
2026-01-16 09:14:01 -0400

    export default InlineMenu;

--- src/components/ui/inline-menu/index.ts
+++ src/components/ui/inline-menu/index.ts
@@ -1,3 +1,4 @@
 
 export { InlineMenuContainer, InlineMenuItem, InlineMenu } from './inline-menu';
 export type { InlineMenuItemDefinition, InlineMenuProps } from './inline-menu';
+export { InlineMenu as default } from './inline-menu';
