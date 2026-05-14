71d2eaf401dfb696242fe229129b64bf51e7cc4b
J. Alex Whitman <51729845+jalexw@users.noreply.github.com>
2026-05-14 10:15:38 -0300

    Remove unused import from Slider.stories.tsx
    Removed unused import statement for 'fn' from story file.

--- src/components/ui/slider/Slider.stories.tsx
+++ src/components/ui/slider/Slider.stories.tsx
@@ -1,8 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-// import { fn } from "storybook/test";
-
 import Slider from "./slider";
-
 import { useArgs } from "storybook/preview-api";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
